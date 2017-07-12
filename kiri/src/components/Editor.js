1f04fc5a1a1d8e55746dfe30f01a6ae4d2a82253
luoop <1337939247@qq.com>
2017-07-13 00:35:01 +0800

    add  update for editor
    use put request method

--- kiri/src/components/Editor.js
+++ kiri/src/components/Editor.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Form from './Form';
+import {validateEmail,validatePhone,validateURL} from '../utils/Validator';
 
 class Editor extends React.Component{
 
@@ -8,34 +9,88 @@ class Editor extends React.Component{
 		super(props);
 
 		this.state = {
-			data: []
+			data: [],
+			submitData:{}
 		}
 	};
 
 	componentWillMount() {
 		const id = this.props.id;
+		let submitData = this.state.submitData;
 		this.properties = this.props.properties;
 		this.props.callback(id).then(item=>{
 			let data = this.state.data;
 
 			this.properties.map(prop=>{
 				let queryResult = {};
-				console.log(item[prop.title]);
+				submitData[prop.title] = item[prop.title];
 				queryResult["value"] = item[prop.title];
 				queryResult["disabled"] = prop.disabled;
 				queryResult["title"] = prop.title;
 				queryResult["type"] = prop.type;
+				submitData["createAt"] = item["createAt"]
 				data.push(queryResult);
 			});
-			this.setState(data);
+			this.setState({data,submitData});
 		})
 	}
 
+	handleChildChange = (key,value,type) => {
+		let msg = "";
+		let submitData = this.state.submitData;
+		switch(type){
+			case "email":
+				if(validateEmail(value)==null){
+					msg = "邮箱格式不正确"
+				}else{
+					submitData[key] = value;
+				}
+				break;
+			case "url":
+				if( validateURL(value)==null ){
+					msg = key+"地址不正确"
+				}else{
+					submitData[key] = value;
+				}
+				break;
+			case "phone":
+				if(validatePhone(value)){
+					msg = "电话格式不正确";
+				}else{
+					submitData[key] = value;
+				}
+				break;
+			default:
+				submitData[key] = value;
+		}
+		this.setState(submitData);
+	};
+
+	submit = () => {
+		let date = new Date();
+		let updateAt = [];
+		updateAt.push(date.getYear()+1900);
+		updateAt.push((date.getMonth()+1)%12);
+		updateAt.push((date.getDay()+9)%30);
+		updateAt.push(date.getHours());
+		updateAt.push(date.getMinutes());
+		updateAt.push(date.getSeconds());
+		let submitData = this.state.submitData;
+		submitData["updateAt"] = updateAt;
+		this.props.submit(this.props.id,submitData);
+		this.setState(submitData);
+	};
+
 	render(){
+
+		console.log(this.state);
 		if(this.state.data){
 			return (
 				<Form
+					id={this.props.id}
 					datas={this.state.data}
+				    onChange={this.handleChildChange}
+				    submit={this.submit}
 				/>
 			)
 		}else{
--- kiri/src/components/Form.js
+++ kiri/src/components/Form.js
@@ -5,12 +5,14 @@ import Button from 'material-ui/Button';
 import TextField from "material-ui/TextField";
 import Paper from 'material-ui/Paper';
 import Grid from 'material-ui/Grid';
+
+import {validateEmail,validatePhone,validateURL} from '../utils/Validator';
 const styleSheet = createStyleSheet("Form",theme=>({
 	root: {
 		flexGrow: 1,
 		marginTop: 30,
 		marginLeft:"12.5%",
-		marginRight:"12.5%",
+		marginRight:"12.5%"
 	},
 	textField:{
 		margin:12,
@@ -21,39 +23,57 @@ const styleSheet = createStyleSheet("Form",theme=>({
 	}
 }));
 
-
+let counter = 0;
 class Form extends React.Component{
 
 	// 构造
-	  constructor(props) {
+	constructor(props) {
 	    super(props);
 	    // 初始状态
-	    this.state = {};
-	  }
+		this.state = {};
+	}
+
+	submit = () => {
+		this.props.submit();
+	};
+
+	handleChange = (e) =>{
+		let title = e.currentTarget.childNodes[0].innerText;
+		let value = e.currentTarget.childNodes[1].firstChild.value;
+		let type = e.currentTarget.childNodes[1].firstChild.type;
+		
+		this.props.onChange(title,value,type);
+
+	};
+	
 
 	render(){
 		const {classes,datas} =this.props;
+		console.log("preData",this.state.preData);
 		return(
 			<Paper className={classes.root}>
 				<Grid container gutter={2}>
-
-					{datas.map(data=>{
-						return(
-							<Grid container>
-								<Grid item xs={6} sm={9}>
-									<TextField label={data.title+":"}
-									           disabled = {data.disabled}
-									           defaultValue = {data.value}
-									           className={classes.textField}
-								               fullWidth={true}
-									           type={data.type}
-									/>
+					{
+						datas.map(data=>{
+							counter += 1;
+							return(
+								<Grid container>
+									<Grid item xs={6} sm={9}>
+										<TextField label={data.title}
+										           disabled = {data.disabled}
+										           defaultValue = {data.value}
+										           className={classes.textField}
+									               fullWidth={true}
+										           type={data.type}
+										           id
+										           onChange={this.handleChange}
+										/>
+									</Grid>
 								</Grid>
-							</Grid>
 						);
 					})}
 				</Grid>
-				<Button className={classes.submit}>保存</Button>
+				<Button onClick={this.submit} className={classes.submit}>保存</Button>
 			</Paper>
 		);
 	}
